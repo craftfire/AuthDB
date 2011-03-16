5ae0a6b1b6c1d9661adb8b6854f08e72aec1f00b
Contex <contex_moh@gmail.com>
2011-03-16 01:16:04 -0700

    Makes all the messages defined global, but they won't get defined as the message before the config is executed in Config.java

--- src/com/gmail/contexmoh/authdb/utils/Messages.java
+++ src/com/gmail/contexmoh/authdb/utils/Messages.java
@@ -16,57 +16,42 @@ public class Messages
 		///////////////////////////////////////////
 		//               welcome
 		///////////////////////////////////////////
-		public static String AuthDB_message_welcome_guest = Config.GetConfigString("messages.AuthDB.welcome.guest", "&4Welcome guest! Please register with /register <password> <email>");
-		public static String AuthDB_message_welcome_user = Config.GetConfigString("messages.AuthDB.welcome.user", "&4Welcome back {PLAYER}! Please login with /login <password>");
+		public static String AuthDB_message_welcome_guest,AuthDB_message_welcome_user;
 		
 		///////////////////////////////////////////
 		//               register
 		///////////////////////////////////////////
-		public static String AuthDB_message_register_success = Config.GetConfigString("messages.AuthDB.register.success", "&4You have been registered!");
-		public static String AuthDB_message_register_failure = Config.GetConfigString("messages.AuthDB.register.failure", "&4Error while registering!");
-		public static String AuthDB_message_register_registered = Config.GetConfigString("messages.AuthDB.register.registered", "&4You are already registered!");
-		public static String AuthDB_message_register_disabled = Config.GetConfigString("messages.AuthDB.register.disabled", "&4Registration not allowed!");
-		public static String AuthDB_message_register_usage = Config.GetConfigString("messages.AuthDB.register.usage", "&4Correct usage is: /register <password> <email>");
+		public static String AuthDB_message_register_success,AuthDB_message_register_failure,AuthDB_message_register_registered,AuthDB_message_register_disabled,AuthDB_message_register_usage;
 		
 		///////////////////////////////////////////
 		//               unregister
 		///////////////////////////////////////////
-		public static String AuthDB_message_unregister_success = Config.GetConfigString("messages.AuthDB.unregister.success", "&2Unregistered successfully!");
-		public static String AuthDB_message_unregister_failure = Config.GetConfigString("messages.AuthDB.unregister.failure", "&4An error occurred while unregistering!");
-		public static String AuthDB_message_unregister_usage = Config.GetConfigString("messages.AuthDB.unregister.usage", "&4Correct usage is: /unregister <password>");
+		public static String AuthDB_message_unregister_success,AuthDB_message_unregister_failure,AuthDB_message_unregister_usage;
 		
 		///////////////////////////////////////////
 		//               login
 		///////////////////////////////////////////
-		public static String AuthDB_message_login_success = Config.GetConfigString("messages.AuthDB.login.success", "&2Password accepted. Welcome!");
-		public static String AuthDB_message_login_failure = Config.GetConfigString("messages.AuthDB.login.failure", "&4Password incorrect, please try again.");
-		public static String AuthDB_message_login_authorized = Config.GetConfigString("messages.AuthDB.login.authorized", "&2Hey, I remember you! You are logged in.");
-		public static String AuthDB_message_login_usage = Config.GetConfigString("messages.AuthDB.login.usage", "&4Correct usage is: /login <password>");
+		public static String AuthDB_message_login_success,AuthDB_message_login_failure,AuthDB_message_login_authorized,AuthDB_message_login_usage;
 		
 		///////////////////////////////////////////
 		//               email
 		///////////////////////////////////////////
-		public static String AuthDB_message_email_required = Config.GetConfigString("messages.AuthDB.email.required", "&4Email required for registration!");
-		public static String AuthDB_message_email_badcharacters = Config.GetConfigString("messages.AuthDB.email.badcharacters", "&4Email contains bad characters: {BADCHARACTERS}!");
+		public static String AuthDB_message_email_required,AuthDB_message_email_badcharacters;
 		
 		///////////////////////////////////////////
 		//               username
 		///////////////////////////////////////////
-		public static String AuthDB_message_username_renamed = Config.GetConfigString("messages.AuthDB.username.renamed", "&2{PLAYER} renamed to {PLAYERNEW} due to bad characters: {BADCHARACTERS}.");
-		public static String AuthDB_message_username_badcharacters = Config.GetConfigString("messages.AuthDB.username.badcharacters", "&4Username contains bad characters: {BADCHARACTERS}!");
+		public static String AuthDB_message_username_renamed,AuthDB_message_username_badcharacters;
 		
 		///////////////////////////////////////////
 		//               password
 		///////////////////////////////////////////
-		public static String AuthDB_message_password_success = Config.GetConfigString("messages.AuthDB.password.success", "&2Password changed successfully!");
-		public static String AuthDB_message_password_failure = Config.GetConfigString("messages.AuthDB.password.failure", "&4Error! Password change failed!");
-		public static String AuthDB_message_password_notregistered = Config.GetConfigString("messages.AuthDB.password.notregistered", "&4Register first!");
-		public static String AuthDB_message_password_usage = Config.GetConfigString("messages.AuthDB.password.usage", "&4Correct usage is: /password <oldpassword> <password>");
+		public static String AuthDB_message_password_success,AuthDB_message_password_failure,AuthDB_message_password_notregistered,AuthDB_message_password_usage;
 		
 		///////////////////////////////////////////
 		//               idle
 		///////////////////////////////////////////
-		public static String AuthDB_message_idle_kick = Config.GetConfigString("messages.AuthDB.idle.kick", "Kicked because you failed to login within {IDLELENGTH} {IDLETIME}.");
+		public static String AuthDB_message_idle_kick;
 		
 	///////////////////////////////////////////
 	//               CraftIRC
@@ -75,28 +60,22 @@ public class Messages
 		///////////////////////////////////////////
 		//               status
 		///////////////////////////////////////////
-		public static String CraftIRC_message_status_join = Config.GetConfigString("messages.CraftIRC.status.guest", "{PLAYER} has joined the server from {PROVINCE}{STATE}, {COUNTRY} ({IP}).");
-		public static String CraftIRC_message_status_quit = Config.GetConfigString("messages.CraftIRC.status.user", "{PLAYER} has quit the server.");
+		public static String CraftIRC_message_status_join,CraftIRC_message_status_quit;
 		
 		///////////////////////////////////////////
 		//               register
 		///////////////////////////////////////////
-		public static String CraftIRC_message_register_success = Config.GetConfigString("messages.CraftIRC.register.success", "{PLAYER} just registered successfully!");
-		public static String CraftIRC_message_register_failure = Config.GetConfigString("messages.CraftIRC.register.failure", "{PLAYER} had some errors while registering!");
-		public static String CraftIRC_message_register_registered = Config.GetConfigString("messages.CraftIRC.register.registered", "{PLAYER} had a lapse in memory and tried to register again.");
+		public static String CraftIRC_message_register_success,CraftIRC_message_register_failure,CraftIRC_message_register_registered;
 		
 		///////////////////////////////////////////
 		//               username
 		///////////////////////////////////////////
-		public static String CraftIRC_message_username_renamed = Config.GetConfigString("messages.CraftIRC.username.renamed", "{PLAYER} renamed to {PLAYERNEW} due to bad characters.");
-		public static String CraftIRC_message_username_badcharacters = Config.GetConfigString("messages.CraftIRC.username.badcharacters", "{PLAYER} was kicked due to bad characters in username!");
+		public static String CraftIRC_message_username_renamed,CraftIRC_message_username_badcharacters;
 		
 		///////////////////////////////////////////
 		//               password
 		///////////////////////////////////////////
-		public static String CraftIRC_message_password_success = Config.GetConfigString("messages.CraftIRC.password.success", "{PLAYER} logged in successfully!");
-		public static String CraftIRC_message_password_failure = Config.GetConfigString("messages.CraftIRC.password.failure", "{PLAYER} tried to login with the wrong password!");
-		
+		public static String CraftIRC_message_password_success,CraftIRC_message_password_failure;
 		
 	public static void SendMessage(String type,Player player,PlayerLoginEvent event){
 		zCraftIRC.SendMessage(type,player);
