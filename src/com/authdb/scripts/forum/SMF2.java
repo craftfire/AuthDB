d1badfeb44fc7733de5ecdead045744c1494841c
Contex <contexmoh@gmail.com>
2011-03-19 15:29:32 +0100

    Changed copyright character to (C)

--- src/com/authdb/scripts/forum/SMF2.java
+++ src/com/authdb/scripts/forum/SMF2.java
@@ -1,4 +1,4 @@
-/**          © Copyright 2011 Contex <contexmoh@gmail.com>
+/**          (C) Copyright 2011 Contex <contexmoh@gmail.com>
 	
 	This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
