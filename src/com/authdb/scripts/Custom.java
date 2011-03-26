26a04c16aaa6e82edc61242b9c96dc192b9a400e
Contex <contexmoh@gmail.com>
2011-03-26 14:50:50 +0100

    Update lisence

--- src/com/authdb/scripts/Custom.java
+++ src/com/authdb/scripts/Custom.java
@@ -1,3 +1,10 @@
+/**          (C) Copyright 2011 Contex <contexmoh@gmail.com>
+	
+This work is licensed under the Creative Commons Attribution-NonCommercial-NoDerivs 3.0 Unported License. 
+To view a copy of this license, visit http://creativecommons.org/licenses/by-nc-nd/3.0/ 
+or send a letter to Creative Commons, 171 Second Street, Suite 300, San Francisco, California, 94105, USA.
+
+**/
 package com.authdb.scripts;
 
   import java.io.UnsupportedEncodingException;
