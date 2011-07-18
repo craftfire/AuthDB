a0acd921edf72bf6c4cd0aef4f494d051d8a11bd
Contex <contexmoh@gmail.com>
2011-07-18 10:38:37 +0200

    Cleaning up playermanager.

--- src/main/java/com/authdb/util/managers/PlayerManager.java
+++ src/main/java/com/authdb/util/managers/PlayerManager.java
@@ -1,18 +1,5 @@
 package com.authdb.util.managers;
 
-import org.bukkit.entity.Player;
-
 public class PlayerManager {
 
-    public void ClearArmorinventory(Player player) {
-        
-    }
-    
-    public static void RenamePlayer(Player player, String name) {
-        player.setDisplayName(name);
-    }
-    
-    public static String GetIP(Player player) {
-        return player.getAddress().getAddress().toString().substring(1);
-    }
 }
